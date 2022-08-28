28273a200fe6c9f3f99a80a1484d2162b086e6a3
Nikhil Gupta <108614432+nikhilgame8@users.noreply.github.com>
2022-08-28 19:10:28 +0530

    Create Para.js

---
+++ src/components/Para.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+export default function Para(props) {
+  const [text, setText]=useState("");
+function textpara(){
+  setText(props.paragraph);
+}
+  return (
+    <div>
+      <button id="click" onClick={textpara()}>Click</button>
+      <p id="para">{text}</p>
+    </div>
+  )
+}
